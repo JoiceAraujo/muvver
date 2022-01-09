c708d33a4e5ea5d678dbee681fdaf99ff44b9884
joice <joice.araujo@tecsinapse.com.br>
2022-01-09 10:45:20 -0300

    [WIP] Setting up: Support folder

---
+++ lib/support/style/application_themes.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+class ApplicationThemes {
+  static final ThemeData theme = ThemeData();
+}
---
+++ lib/support/utils/mobile_router.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class MobileRouter {
+  static const String initialRoute = '/';
+
+  static final Map<String, WidgetBuilder> routes = {
+    '/': (_) => Container(),
+  };
+}
