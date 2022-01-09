cc916cb3cc52745c658b0aaca4261d75bf38cbe5
joice <joice.araujo@tecsinapse.com.br>
2022-01-09 16:58:19 -0300

    [WIP] Adding constant values

--- lib/support/utils/constants.dart
+++ lib/support/utils/constants.dart
@@ -1,1 +1,12 @@
-class Constants {}
+class Constants {
+  // Assets
+  static const String imagesPath = 'assets/images';
+
+  // Svg's
+  static const String boxIconPath = '$imagesPath/svg/box.svg';
+  static const String deliveryTruckIconPath = '$imagesPath/svg/delivery_truck.svg';
+  static const String logoPath = '$imagesPath/svg/logo.svg';
+
+  // Png's
+  static const String avatarPath = '$imagesPath/avatar.png';
+}
