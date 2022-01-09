d1f07197bb1b955495e5de3a3e169806ae52317b
joice <joice.araujo@tecsinapse.com.br>
2022-01-09 16:59:48 -0300

    [WIP] Updating application style

--- lib/support/style/application_colors.dart
+++ lib/support/style/application_colors.dart
@@ -1,1 +1,8 @@
-class ApplicationColors {}
+import 'package:flutter/material.dart';
+
+class ApplicationColors {
+  static const Color darkGray = Color.fromRGBO(34, 34, 34, 1);
+  static const Color lightGray = Color.fromRGBO(53, 55, 64, 1);
+  static const Color white = Color.fromRGBO(255, 255, 255, 1);
+  static const Color gray = Color.fromRGBO(34, 34, 34, 0.32);
+}
--- lib/support/style/application_typography.dart
+++ lib/support/style/application_typography.dart
@@ -1,1 +1,30 @@
-class ApplicationTypography {}
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import 'application_colors.dart';
+
+class ApplicationTypography {
+  static final TextStyle titilliumWeb20SemiBoldWhite = GoogleFonts.titilliumWeb(
+    fontSize: 20,
+    fontWeight: FontWeight.w600,
+    color: ApplicationColors.white,
+  );
+
+  static final TextStyle titilliumWeb14RegularWhite = GoogleFonts.titilliumWeb(
+    fontSize: 14,
+    fontWeight: FontWeight.w400,
+    color: ApplicationColors.white,
+  );
+
+  static final TextStyle titilliumWeb16Regular = GoogleFonts.titilliumWeb(
+    fontSize: 16,
+    fontWeight: FontWeight.w400,
+    color: ApplicationColors.gray,
+  );
+
+  static final TextStyle titilliumWeb20Regular = GoogleFonts.titilliumWeb(
+    fontSize: 20,
+    fontWeight: FontWeight.w400,
+    color: ApplicationColors.darkGray,
+  );
+}
