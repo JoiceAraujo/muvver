4469a49e2f27a37960a83e6ac63dea34a883954e
joice <joice.araujo@tecsinapse.com.br>
2022-01-11 08:38:50 -0300

    [WIP] Updating application style

--- lib/support/style/application_themes.dart
+++ lib/support/style/application_themes.dart
@@ -1,5 +1,16 @@
 import 'package:flutter/material.dart';
 
+import 'application_colors.dart';
+import 'application_typography.dart';
+
 class ApplicationThemes {
-  static final ThemeData theme = ThemeData();
+  static final ThemeData theme = ThemeData(
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+        primary: ApplicationColors.green,
+        fixedSize: const Size.fromHeight(48),
+        textStyle: ApplicationTypography.titilliumWeb16BoldWhite,
+      ),
+    ),
+  );
 }
--- lib/support/style/application_typography.dart
+++ lib/support/style/application_typography.dart
@@ -16,15 +16,45 @@ class ApplicationTypography {
     color: ApplicationColors.white,
   );
 
-  static final TextStyle titilliumWeb16Regular = GoogleFonts.titilliumWeb(
+  static final TextStyle titilliumWeb16RegularGray32 = GoogleFonts.titilliumWeb(
     fontSize: 16,
+    fontWeight: FontWeight.w400,
+    color: ApplicationColors.gray32,
+  );
+
+  static final TextStyle titilliumWeb20RegularGray = GoogleFonts.titilliumWeb(
+    fontSize: 20,
     fontWeight: FontWeight.w400,
     color: ApplicationColors.gray,
   );
 
-  static final TextStyle titilliumWeb20Regular = GoogleFonts.titilliumWeb(
+  static final TextStyle titilliumWeb12BoldGray = GoogleFonts.titilliumWeb(
+    fontSize: 12,
+    fontWeight: FontWeight.w700,
+    color: ApplicationColors.gray,
+  );
+
+  static final TextStyle titilliumWeb16RegularWhite = GoogleFonts.titilliumWeb(
+    fontSize: 16,
+    fontWeight: FontWeight.w400,
+    color: ApplicationColors.white,
+  );
+
+  static final TextStyle titilliumWeb20RegularWhite = GoogleFonts.titilliumWeb(
     fontSize: 20,
     fontWeight: FontWeight.w400,
-    color: ApplicationColors.darkGray,
+    color: ApplicationColors.white,
+  );
+
+  static final TextStyle titilliumWeb16BoldGray = GoogleFonts.titilliumWeb(
+    fontSize: 16,
+    fontWeight: FontWeight.w700,
+    color: ApplicationColors.gray,
+  );
+
+  static final TextStyle titilliumWeb16BoldWhite = GoogleFonts.titilliumWeb(
+    fontSize: 16,
+    fontWeight: FontWeight.w700,
+    color: ApplicationColors.white,
   );
 }
