7d7be823bf85d60b174e0c2fd6a2e1e2459895d5
joice <joice.araujo@tecsinapse.com.br>
2022-01-11 08:42:18 -0300

    [FINISHED] Updating MobileRouter

--- lib/support/utils/mobile_router.dart
+++ lib/support/utils/mobile_router.dart
@@ -2,11 +2,14 @@ import 'package:flutter/material.dart';
 
 import '../../features/home/home/home_view_controller.dart';
 import '../../features/home/home_factory.dart';
+import '../../features/transport_type/transport_type/transport_type_view_controller.dart';
+import '../../features/transport_type/transport_type_factory.dart';
 
 class MobileRouter {
   static const String initialRoute = HomeViewController.route;
 
   static final Map<String, WidgetBuilder> routes = {
     HomeViewController.route: (_) => HomeFactory.home(),
+    TransportTypeViewController.route: (_) => TransportTypeFactory.transportType(),
   };
 }
