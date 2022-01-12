ed492aa61bcd2be698ccde8a21056c74449c46bf
JoiceAraujo <joicearaujo10@gmail.com>
2022-01-11 22:47:59 -0300

    [WIP] Updating Constants file

--- lib/support/utils/constants.dart
+++ lib/support/utils/constants.dart
@@ -3,6 +3,7 @@ class Constants {
   static const String imagesPath = 'assets/images';
 
   // Svg's
+  static const String addCircleOutlineIconPath = '$imagesPath/svg/add_circle_outline.svg';
   static const String airplaneIconPath = '$imagesPath/svg/airplane.svg';
   static const String bikeIconPath = '$imagesPath/svg/bike.svg';
   static const String boatIconPath = '$imagesPath/svg/boat.svg';
@@ -11,6 +12,7 @@ class Constants {
   static const String carIconPath = '$imagesPath/svg/car.svg';
   static const String deliveryTruckIconPath = '$imagesPath/svg/delivery_truck.svg';
   static const String logoPath = '$imagesPath/svg/logo.svg';
+  static const String mapPointerIconPath = '$imagesPath/svg/map_pointer.svg';
   static const String motorcycleIconPath = '$imagesPath/svg/motorcycle.svg';
   static const String trainIconPath = '$imagesPath/svg/train.svg';
   static const String truckIconPath = '$imagesPath/svg/truck.svg';
