2cd8aac405a6efcad729f4f9d59b8ce96b75c1fc
JoiceAraujo <joicearaujo10@gmail.com>
2022-01-13 16:32:39 -0300

    [WIP] Updating Constants file

--- lib/support/utils/constants.dart
+++ lib/support/utils/constants.dart
@@ -5,15 +5,21 @@ class Constants {
   // Svg's
   static const String addCircleOutlineIconPath = '$imagesPath/svg/add_circle_outline.svg';
   static const String airplaneIconPath = '$imagesPath/svg/airplane.svg';
+  static const String backpackIconPath = '$imagesPath/svg/backpack.svg';
+  static const String bigBoxIconPath = '$imagesPath/svg/big_box.svg';
+  static const String bigSuitcaseIconPath = '$imagesPath/svg/big_suitcase.svg';
   static const String bikeIconPath = '$imagesPath/svg/bike.svg';
   static const String boatIconPath = '$imagesPath/svg/boat.svg';
+  static const String bookIconPath = '$imagesPath/svg/book.svg';
   static const String boxIconPath = '$imagesPath/svg/box.svg';
   static const String busIconPath = '$imagesPath/svg/bus.svg';
   static const String carIconPath = '$imagesPath/svg/car.svg';
   static const String deliveryTruckIconPath = '$imagesPath/svg/delivery_truck.svg';
+  static const String envelopeIconPath = '$imagesPath/svg/envelope.svg';
   static const String logoPath = '$imagesPath/svg/logo.svg';
   static const String mapPointerIconPath = '$imagesPath/svg/map_pointer.svg';
   static const String motorcycleIconPath = '$imagesPath/svg/motorcycle.svg';
+  static const String shoeBoxIconPath = '$imagesPath/svg/shoe_box.svg';
   static const String trainIconPath = '$imagesPath/svg/train.svg';
   static const String truckIconPath = '$imagesPath/svg/truck.svg';
   static const String vanIconPath = '$imagesPath/svg/van.svg';
